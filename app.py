1fa3c720c5e53426d77e4d858b47877406cd2b82
URSULINA <iranildoalves7@gmail.com>
2024-04-23 10:31:31 -0300

    installing KivyMD

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from kivy.core.window import Window
-from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
 
-# TO LOAD THE INTERFACE FILLE
-Builder.load_file("interface.kv")
 
-class Interface(Widget):
-    nome = ObjectProperty(None)
-    
-    def press(self):
-        print(self.nome.text)
-
-class Start(App):
+class MainApp(MDApp):
     def build(self):
-        return Interface()
+        return MDLabel(text="Hello, World", halign="center")
+
 
 if __name__ == "__main__":
-    Start().run()
+    MainApp().run()
--- run-app.sh
+++ run-app.sh
@@ -1,7 +1,9 @@
 sudo xhost +local:docker;
 sudo docker run --rm --name adutora-container \
     -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=$XAUTHORITY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.config/app:/root/.config/app \
+    -v $(pwd):/app \
     --device /dev/input \
     adutora-image;

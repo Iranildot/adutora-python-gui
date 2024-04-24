04ca4b8b6cb78ab6100fb390ccdef1aa252b848f
URSULINA <iranildoalves7@gmail.com>
2024-04-23 21:43:58 -0300

    Small update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 
+
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Orange"
-
+        self.theme_cls.primary_palette = "Green"
         return Builder.load_file("interface.kv")
 
 if __name__ == "__main__":
